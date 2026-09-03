25700c049094aaa91e1dc0270bee75501e8c8a7a
redsun352 <greedyy3843@gmail.com>
2026-09-03 22:38:17 +0300

    fix(database): export PrismaClient runtime value

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-export type { PrismaClient, Prisma } from "@prisma/client";
+export { PrismaClient } from "@prisma/client";
+export type { Prisma } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
